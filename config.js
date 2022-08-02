5cddf9f3835c9af1a446645ab4b27aadeaf88056
ᴢɪxʏ.ᴇᴀꜱᴘᴏʀᴛ.xxx <107806210+Ikymd@users.noreply.github.com>
2022-08-02 18:20:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -40,7 +40,7 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = '@XznBotz-MD II'
+  var sticker_name = '@チカボット「Chika 」'
   var sticker_author = 'Whatsapp-Bot'
 } else {
   var sticker_name = stickerpack.spackname
@@ -58,7 +58,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = '© XznBotz-MD II'
+global.wm = '© チカボット「Chika 」'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
